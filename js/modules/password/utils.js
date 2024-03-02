23cd354d144dc30e6ba66c79e5c2443fc3aa43ae
Matheus Levi <matheuslevit@gmail.com>
2024-03-02 17:29:02 -0300

    initial commit

--- js/modules/password/utils.js
+++
@@ -1,7 +0,0 @@
-export function getRandomNumber(min, max) {
-    return Math.floor(Math.random() * ((min - 1) - max + 1)) + max;
-}
-
-export function getElement(selector) {
-    return document.querySelector(selector);
-}
