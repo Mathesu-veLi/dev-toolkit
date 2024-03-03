085c010e8d5b0ae389ceac2431f2e8db31bbad7a
Matheus <matheuslevit@gmail.com>
2024-03-03 18:42:41 -0300

    test cpf generation

--- src/classes/cpf.test.ts
+++ src/classes/cpf.test.ts
@@ -11,3 +11,23 @@ describe("Validate CPF", () => {
     expect(cpf.isValid()).toBeTruthy();
   });
 });
+
+describe("Generate CPF", () => {
+  test("should return a cpf with 11 numbers", () => {
+    const cpf = new Cpf("").generate();
+    const formattedCpf = cpf.replace(/\D+/g, "");
+    
+    expect(formattedCpf.length).toBe(11);
+  });
+
+  test("should return a valid cpf", () => {
+    const cpf = new Cpf("");
+    cpf.generate();
+    expect(cpf.isValid()).toBeTruthy();
+  });
+
+  test("should return a formatted CPF", () => {
+    const cpf = new Cpf("").generate();
+    expect(cpf.match(/(\d{3}\.){2}\d{3}-\d{2}/)).not.toBeNull();
+  });
+});
