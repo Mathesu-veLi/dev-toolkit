cf83f72d2052bfa5d498f265cb9acabac62de747
Matheus <matheuslevit@gmail.com>
2024-03-03 14:57:03 -0300

    creates the home page

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+export function Home() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="flex flex-col justify-center items-center gap-4 border p-6 rounded-sm">
+        <h1 className="tracking-wider font-semibold text-xl">Dev Toolkit</h1>
+        <p className="max-w-52 leading-relaxed tracking-wider">
+          The website to solve your daily problems as a dev!
+        </p>
+      </div>
+    </div>
+  );
+}
