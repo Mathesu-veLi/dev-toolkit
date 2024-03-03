fff35816ce18a872985c8676e3f77904ee78cc6f
Matheus <matheuslevit@gmail.com>
2024-03-03 15:13:40 -0300

    set cpf with a local state

--- src/components/Cpf.tsx
+++ src/components/Cpf.tsx
@@ -1,14 +1,15 @@
+import { useState } from "react";
 import { Button } from "./ui/button";
 
 export function Cpf() {
+  const [cpf, setCpf] = useState<string>("000.000.000-00");
+
   return (
     <div className="flex justify-center items-center h-full">
       <div className="flex flex-col gap-4 justify-center items-center border p-5">
         <h1 className="text-xl font-semibold tracking-wider">CPF Generator</h1>
-        <p id="cpf" className="text-lg">
-          000.000.000-00
-        </p>
-        <Button>Generate</Button>
+        <p className="text-lg">{cpf}</p>
+        <Button onClick={() => setCpf(generateCpf())}>Generate</Button>
       </div>
     </div>
   );
