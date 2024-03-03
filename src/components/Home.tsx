70f4e829217c8ae199f141b6170622f9bed27c69
Matheus <matheuslevit@gmail.com>
2024-03-03 15:04:32 -0300

    edit home page style in desktops

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,10 @@ export function Home() {
   return (
     <div className="flex justify-center items-center h-full">
       <div className="flex flex-col justify-center items-center gap-4 border p-6 rounded-sm">
-        <h1 className="tracking-wider font-semibold text-xl">Dev Toolkit</h1>
-        <p className="max-w-52 leading-relaxed tracking-wider">
+        <h1 className="tracking-wider font-semibold text-xl lg:text-2xl">
+          Dev Toolkit
+        </h1>
+        <p className="max-w-52 leading-relaxed tracking-wider lg:max-w-none">
           The website to solve your daily problems as a dev!
         </p>
       </div>
