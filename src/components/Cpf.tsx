0e0b5fad113e54f28459d3cce92e38db77e726e1
Matheus <matheuslevit@gmail.com>
2024-03-03 15:11:36 -0300

    creates the visual of the cpf generator

--- src/components/Cpf.tsx
+++ src/components/Cpf.tsx
@@ -1,3 +1,15 @@
+import { Button } from "./ui/button";
+
 export function Cpf() {
-  return <h1>Cpf</h1>;
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="flex flex-col gap-4 justify-center items-center border p-5">
+        <h1 className="text-xl font-semibold tracking-wider">CPF Generator</h1>
+        <p id="cpf" className="text-lg">
+          000.000.000-00
+        </p>
+        <Button>Generate</Button>
+      </div>
+    </div>
+  );
 }
