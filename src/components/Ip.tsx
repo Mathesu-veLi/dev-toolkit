29dc5a43cc7a05ddc6afd160dab529520660fce3
Matheus Levi <matheuslevit@gmail.com>
2024-03-04 20:03:45 -0300

    creates the visual of the ip generator

--- src/components/Ip.tsx
+++ src/components/Ip.tsx
@@ -1,3 +1,16 @@
+import { useState } from "react";
+import { Button } from "./ui/button";
+
 export function Ip() {
-    return <h1>Ip</h1>
+  const [ip, setIp] = useState("255.255.255.255");
+
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="flex flex-col gap-4 justify-center items-center border p-5 w-80 rounded-sm">
+        <h1 className="text-xl font-semibold tracking-wider">IP Generator</h1>
+        <p className="text-lg">{ip}</p>
+        <Button onClick={() => setIp(generateIp())}>Generate</Button>
+      </div>
+    </div>
+  );
 }
