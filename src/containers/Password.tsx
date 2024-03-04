82f4f81b2067299ca35dc4eb7f63f5d37e6a3986
Matheus Levi <matheuslevit@gmail.com>
2024-03-04 20:30:57 -0300

    creates a basic visual of the password generator

--- src/containers/Password.tsx
+++ src/containers/Password.tsx
@@ -1,8 +1,18 @@
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
 export function Password() {
+  const [password, setPassword] = useState<string>("passwoRd123!");
   return (
     <div className="flex justify-center items-center h-full">
-      <div className="grid gap-10">
-        
+      <div className="flex flex-col gap-4 justify-center items-center border p-5 w-80 rounded-sm">
+        <h1 className="text-xl font-semibold tracking-wider">
+          Password Generator
+        </h1>
+        <p className="text-lg">{password}</p>
+        <Button onClick={() => setPassword(generatePassword())}>
+          Generate
+        </Button>
       </div>
     </div>
   );
